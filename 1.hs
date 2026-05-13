a9c689a72c875ad49b21dd5f10c7cc21213bbd6f
miguel-jaeger <miguel.jaeger@gmail.com>
2026-05-13 15:11:33 -0500

    add variant to ex 1

--- 1.hs
+++ 1.hs
@@ -1,8 +1,19 @@
+-- Variante 1: Sumar los tres primeros números de una lista
 sumarTres :: [Int] -> Int
 sumarTres (x:y:z:_) = x + y + z
 sumarTres _         = 0
+
+-- Variante 2: Sumar los tres primeros números de una lista utilizando foldl
+sumarTresPrimerosAlt :: [Int] -> Int
+sumarTresPrimerosAlt xs
+    | length xs >= 3 = sum (take 3 xs)
+    | otherwise      = 0
+
 main :: IO ()
 main = do
-    let numeros = [1, 2, 3, 4, 5]
+    --let numeros = [1, 2, 3, 4, 5]
+    putStrLn "Escriba la lista de numeros separadas por espacios:"
+    input <- getLine
+    let numeros = map read (words input) :: [Int]
     let resultado = sumarTres numeros
     putStrLn $ "La suma de los tres primeros números es: " ++ show resultado
--- suma.hs
+++ suma.hs
@@ -5,5 +5,8 @@ main :: IO ()
 main = do
     --let numeros = [3, 5, 7, 9]
     let numeros = []
+    putStrLn "Escriba la lista de numeros separadas por espacios:"
+    input <- getLine
+    let numeros = map read (words input) :: [Int]
     let resultado = sumarDosPrimeros numeros
     putStrLn $ "La suma de los dos primeros números es: " ++ show resultado
