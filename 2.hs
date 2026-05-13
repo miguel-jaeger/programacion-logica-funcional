8d24330174c6f6b602b8fc9a124e357b6b5db4f4
miguel-jaeger <miguel.jaeger@gmail.com>
2026-05-13 15:13:27 -0500

    add variant to ex 2

--- 2.hs
+++ 2.hs
@@ -1,5 +1,10 @@
 soloAdultos :: [Int] -> [Int]
 soloAdultos edades = [x | x <- edades, x >= 18]
+
+-- Variante con filter
+soloAdultosFilter :: [Int] -> [Int]
+soloAdultosFilter edades = filter (>= 18) edades
+
 main :: IO ()
 main = do
     let edades = [15, 22, 17, 30, 12, 18]
