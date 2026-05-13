d1cd0336ab2813a9ab7defa071b87a2ea2e69c73
miguel-jaeger <miguel.jaeger@gmail.com>
2026-05-13 15:17:07 -0500

    add variant to ex 3

--- 3.hs
+++ 3.hs
@@ -4,6 +4,9 @@ miLongitud (_:xs) = 1 + miLongitud xs             -- Caso Recursivo
 
 main :: IO ()
 main = do
-    let lista = [1, 2, 3, 4, 5]
+    --let lista = [1, 2, 3, 4, 5]
+    putStrLn "Escriba la lista de numeros separadas por espacios:"
+    input <- getLine
+    let lista = map read (words input) :: [Int]
     let longitud = miLongitud lista
     putStrLn $ "La longitud de la lista es: " ++ show longitud
