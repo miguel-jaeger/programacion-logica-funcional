fda03dc9137e6860c12160a1a69ec4ed40a36357
miguel-jaeger <miguel.jaeger@gmail.com>
2026-05-13 15:21:00 -0500

    add variant to ex 7

--- 7.hs
+++ 7.hs
@@ -1,8 +1,16 @@
 invertir :: [a] -> [a]
 invertir []     = []
 invertir (x:xs) = invertir xs ++ [x]  -- Uso de concatenación
+
+-- variante con foldr
+invertirFold :: [a] -> [a]
+invertirFold xs = foldr (:) [] xs
+
 main :: IO ()
 main = do
-    let lista = [1, 2, 3, 4, 5]
+    --let lista = [1, 2, 3, 4, 5]
+    putStrLn "Escriba la lista de numeros separadas por espacios:"
+    input <- getLine
+    let lista = map read (words input) :: [Int]
     let listaInvertida = invertir lista
     putStrLn $ "La lista invertida es: " ++ show listaInvertida
