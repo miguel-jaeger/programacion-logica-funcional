6f41129eca828ba957d94fd35efcd2f60f32452a
miguel-jaeger <miguel.jaeger@gmail.com>
2026-05-13 15:22:06 -0500

    add variant to ex 8

--- 8.hs
+++ 8.hs
@@ -2,8 +2,16 @@ import Data.Char (isDigit)
 esNumerica :: String -> Bool
 esNumerica [] = False
 esNumerica s  = all isDigit s    -- Uso de all de alto nivel
+
+-- variante con foldr
+esNumericaFold :: String -> Bool
+esNumericaFold s = foldr (\c acc -> isDigit c && acc) True s
+
 main :: IO ()
 main = do
-    let cadena = "12345"
+    --let cadena = "12345"
+    putStrLn "Escriba una cadena para verificar si es numérica:"
+    input <- getLine
+    let cadena = input
     let resultado = esNumerica cadena
     putStrLn $ "¿La cadena es numérica? " ++ show resultado
