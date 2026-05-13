4acfbed4d2a405284314fcdbe29af5aa738ff874
miguel-jaeger <miguel.jaeger@gmail.com>
2026-05-13 15:18:07 -0500

    add variant to ex 4

--- 4.hs
+++ 4.hs
@@ -4,6 +4,9 @@ enMayusculas :: String -> String
 enMayusculas cadena = map toUpper cadena
 main :: IO ()
 main = do
-    let texto = "hola mundo"
+    --let texto = "hola mundo"
+    putStrLn "Escriba un texto para convertirlo a mayúsculas:"
+    input <- getLine
+    let texto = input
     let resultado = enMayusculas texto
     putStrLn $ "El texto en mayúsculas es: " ++ resultado
