94f7ea10e90d39960aeddd907c25c273084cbea8
miguel-jaeger <miguel.jaeger@gmail.com>
2026-05-13 15:16:21 -0500

    add variant to ex 2

--- 2.hs
+++ 2.hs
@@ -7,6 +7,9 @@ soloAdultosFilter edades = filter (>= 18) edades
 
 main :: IO ()
 main = do
-    let edades = [15, 22, 17, 30, 12, 18]
+    --let edades = [15, 22, 17, 30, 12, 18]
+    putStrLn "Escriba la lista de edades separadas por espacios:"
+    input <- getLine
+    let edades = map read (words input) :: [Int]
     let adultos = soloAdultos edades
     putStrLn $ "Las edades de los adultos son: " ++ show adultos
--- 5.hs
+++ 5.hs
@@ -1,5 +1,11 @@
 multiplicarLista :: [Int] -> Int
 multiplicarLista lista = foldl (*) 1 lista   -- 1 es la identidad 
+
+-- Variante con recursión
+multiplicarListaRec :: [Int] -> Int
+multiplicarListaRec []     = 1
+multiplicarListaRec (x:xs) = x * multiplicarListaRec xs
+
 main :: IO ()
 main = do
     let lista = [1, 2, 3, 4, 5]
