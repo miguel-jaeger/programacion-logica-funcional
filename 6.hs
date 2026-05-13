94e38df4cedf7379d958b1dd5870f739dd0646ec
miguel-jaeger <miguel.jaeger@gmail.com>
2026-05-13 15:19:39 -0500

    add variant to ex 6

--- 6.hs
+++ 6.hs
@@ -1,7 +1,15 @@
 nombresCortos :: [String] -> [(String, Int)]
 nombresCortos lista = [(n, length n) | n <- lista, length n < 5]
+
+-- Variante con filter
+nombresCortosFilter :: [String] -> [(String, Int)]
+nombresCortosFilter lista = map (\n -> (n, length n)) (filter (\n -> length n < 5) lista)
+
 main :: IO ()
 main = do   
-    let nombres = ["Ana", "Luis", "Pedro", "Eva", "Juan"]
+   -- let nombres = ["Ana", "Luis", "Pedro", "Eva", "Juan"]
+    putStrLn "Escriba la lista de nombres separadas por espacios:"
+    input <- getLine
+    let nombres = map read (words input) :: [String]
     let resultado = nombresCortos nombres
     putStrLn $ "Los nombres cortos y sus longitudes son: " ++ show resultado
