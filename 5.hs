73c6de051637a01590401881c79718b5110d347d
miguel-jaeger <miguel.jaeger@gmail.com>
2026-05-13 15:18:29 -0500

    add variant to ex 5

--- 5.hs
+++ 5.hs
@@ -8,6 +8,9 @@ multiplicarListaRec (x:xs) = x * multiplicarListaRec xs
 
 main :: IO ()
 main = do
-    let lista = [1, 2, 3, 4, 5]
+    --let lista = [1, 2, 3, 4, 5]
+    putStrLn "Escriba la lista de numeros separadas por espacios:"
+    input <- getLine
+    let lista = map read (words input) :: [Int]
     let resultado = multiplicarLista lista
     putStrLn $ "El producto de la lista es: " ++ show resultado
