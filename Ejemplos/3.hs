cba91597e443b16ddec72f5460036b7b720a9d0d
miguel-jaeger <miguel.jaeger@gmail.com>
2026-07-01 11:02:12 -0500

    cambiado ejercicio 3

--- Ejemplos/3.hs
+++ Ejemplos/3.hs
@@ -13,7 +13,9 @@ main = do
     -- Ejemplo de uso del predicado de pertenencia
    -- let arbol = Nodo 10 (Nodo 5 Vacio Vacio) (Nodo 15 Vacio Vacio)
    -- Arbol de la presentación:
-    let arbol = Nodo 10 (Nodo 5 (Nodo 2 Vacio Vacio) (Nodo 7 Vacio Vacio)) (Nodo 15 (Nodo 12 Vacio Vacio) (Nodo 20 Vacio Vacio))
+    let arbol = Nodo 10 
+                (Nodo 5 (Nodo 2 Vacio Vacio) (Nodo 7 Vacio Vacio)) 
+                (Nodo 15 (Nodo 12 Vacio Vacio) (Nodo 20 Vacio Vacio))
     putStrLn $ "¿Existe el valor 5 en el árbol? " ++ show (existe 5 arbol)
     putStrLn $ "¿Existe el valor 20 en el árbol? " ++ show (existe 20 arbol)
     putStrLn "Presione una tecla para finalizar..."
