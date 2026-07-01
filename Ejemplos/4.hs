cd7240e42dc6bc92fb9c687c9de072c85db86047
miguel-jaeger <miguel.jaeger@gmail.com>
2026-07-01 11:12:13 -0500

    actualización del ej4

--- Ejemplos/4.hs
+++ Ejemplos/4.hs
@@ -1,4 +1,4 @@
-categoriasPrioridad = ["Salud", "Alimentos", "Seguridad"]
+--categoriasPrioridad = ["Salud", "Alimentos", "Seguridad"]
 
 puedeDespachar :: Int -> String -> [String] -> Bool
 puedeDespachar stock cat lista = stock > 0 && elem cat lista
@@ -7,6 +7,10 @@ main :: IO ()
 main = do
     putStrLn "Ingrese el stock disponible: "
     stock <- readLn :: IO Int
+    putStrLn "Ingrese la lista de categorias disponibles: "
+    categorias <- getLine
+    
+    let categoriasPrioridad = words categorias
     putStrLn "Ingrese la categoría del producto (Salud, Alimentos, Seguridad): "
     categoria <- getLine
     let resultado = puedeDespachar stock categoria categoriasPrioridad
