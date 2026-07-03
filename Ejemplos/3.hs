33dea02391f2606778648788c688c6c135bf8346
DOCENTE - MIGUEL JAEGER RODRIGUEZ LAZO <C32007@utp.edu.pe>
2026-07-03 11:36:18 -0500

    Actualizados ejercicios

--- Ejemplos/3.hs
+++ Ejemplos/3.hs
@@ -14,10 +14,18 @@ main = do
    -- let arbol = Nodo 10 (Nodo 5 Vacio Vacio) (Nodo 15 Vacio Vacio)
    -- Arbol de la presentación:
     let arbol = Nodo 10 
-                (Nodo 5 (Nodo 2 Vacio Vacio) (Nodo 7 Vacio Vacio)) 
-                (Nodo 15 (Nodo 12 Vacio Vacio) (Nodo 20 Vacio Vacio))
-    putStrLn $ "¿Existe el valor 5 en el árbol? " ++ show (existe 5 arbol)
-    putStrLn $ "¿Existe el valor 20 en el árbol? " ++ show (existe 20 arbol)
+                (Nodo 5 
+                    (Nodo 2 Vacio Vacio) (Nodo 7 Vacio Vacio)) 
+                (Nodo 15 
+                    (Nodo 12 (Nodo 8 (Nodo 6 Vacio Vacio) (Nodo 4 Vacio Vacio) ) Vacio) 
+                    (Nodo 20 Vacio Vacio))
+    putStrLn "Ingrese nodo a buscar"
+    input<-getLine
+    let valor = read input::Int
+
+    putStrLn $ "¿Existe el valor: "++ show valor ++" en el árbol? " ++ show (existe valor arbol)
+    --putStrLn $ "¿Existe el valor 20 en el árbol? " ++ show (existe 20 arbol)
+
     putStrLn "Presione una tecla para finalizar..."
     _ <- getLine
     return ()
