6aa5e2e7dd21137804c5199b5e6d198244b0ff67
DOCENTE - MIGUEL JAEGER RODRIGUEZ LAZO <C32007@utp.edu.pe>
2026-07-08 19:41:15 -0500

    change ex5.1

--- Ejemplos/5-1.hs
+++ Ejemplos/5-1.hs
@@ -1,5 +1,5 @@
 prioridad :: [(String, Int)]
-prioridad = [("Salud", 10), ("Alimentos", 5)]
+prioridad = [("Salud", 10), ("Alimentos", 25)]
 
 esDespachable :: String -> Int -> Bool
 esDespachable categoria stock
@@ -10,8 +10,17 @@ esDespachable categoria stock
 
 main :: IO ()
 main = do
-    let categoriaProducto = "Alimentos"
-    let stockProducto = 10
+    --let categoriaProducto = "Alimentos"
+    putStrLn "Diga el producto"
+    input<-getLine
+    let categoriaProducto =input
+
+    --let stockProducto = 10
+    putStrLn "Diga stock solicitado"
+    input2<-getLine
+    let stockProducto =read input2::Int
+   
+
     let msg = if esDespachable categoriaProducto stockProducto
                 then "Si"
                 else "No"
