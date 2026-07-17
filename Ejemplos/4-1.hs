7fc8d78d16409eb09ff4cd71508943634fed5ffd
miguel-jaeger <84285594+miguel-jaeger@users.noreply.github.com>
2026-07-17 10:51:33 -0500

    Uncomment 'existe' function in Arbol data structure

--- Ejemplos/4-1.hs
+++ Ejemplos/4-1.hs
@@ -1,10 +1,10 @@
 data Arbol a = Vacio | Nodo a (Arbol a) (Arbol a) deriving (Show, Eq)
 
-{--existe :: Eq a => a -> Arbol a -> Bool
+existe :: Eq a => a -> Arbol a -> Bool
 existe _ Vacio = False
 existe x (Nodo valor izq der)
     | x == valor = True                -- Éxito
-    | otherwise  = existe x izq || existe x der -- Disyunción lógica (Backtracking)--}
+    | otherwise  = existe x izq || existe x der -- Disyunción lógica (Backtracking)
 
 contar :: Eq a => a -> Arbol a -> Int
 contar _ Vacio = 0
@@ -18,11 +18,16 @@ main = do
     --let arbol = Nodo 5 (Nodo 3 Vacio Vacio) (Nodo 7 (Nodo 4 Vacio Vacio) (Nodo 5 (Nodo 4 (Nodo 4 Vacio Vacio) (Nodo 4 Vacio Vacio)) (Nodo 7 Vacio Vacio)))
     let arbol = Nodo 1 (Nodo 2 (Nodo 4 Vacio Vacio) (Nodo 5 (Nodo 4 Vacio Vacio)  Vacio) ) (Nodo 3 (Nodo 6 (Nodo 9 (Nodo 4 Vacio Vacio)  (Nodo 4 Vacio Vacio) )  (Nodo 10 (Nodo 4 Vacio Vacio)  Vacio) )  (Nodo 7 (Nodo 4 Vacio Vacio)  Vacio) ) 
     -- Representación gráfica del árbol:
-    --         5
-    --        / \   
-    --       3   7
-    --          / \
-    --         4   4
+    --             1
+    --        /         \   
+    --       2           3
+    --      / \          /   \
+    --     4   5        6       7
+    --           \      / \      \    
+    --             4   9    10    4
+    --                /      \   
+    --               4       4  
+                      
    
     --let elemento = 7
     putStrLn("Diga elemento a buscar")
